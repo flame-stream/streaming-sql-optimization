be6b3d4ff343ca855e2d534285a0e479482b77d2
Nikita Sokolov <faucct@yandex-team.ru>
2021-07-15 09:33:46 +0300

    Formatting

--- agents/src/main/java/com/flamestream/optimizer/sql/agents/CostEstimatorImpl.java
+++ agents/src/main/java/com/flamestream/optimizer/sql/agents/CostEstimatorImpl.java
@@ -9,20 +9,16 @@ import org.apache.beam.vendor.calcite.v1_20_0.org.apache.calcite.rel.metadata.Ch
 import org.apache.beam.vendor.calcite.v1_20_0.org.apache.calcite.rel.metadata.RelMetadataProvider;
 import org.apache.beam.vendor.calcite.v1_20_0.org.apache.calcite.rel.metadata.RelMetadataQuery;
 
-import java.util.Map;
-
 public class CostEstimatorImpl implements CostEstimator {
-
     @Override
-    public RelOptCost getCumulativeCost(BeamRelNode rel,
-                                           ImmutableList<RelMetadataProvider> providers, RelMetadataQuery mq) {
-        rel.getCluster().setMetadataProvider(
-                ChainedRelMetadataProvider.of(
-                        org.apache.beam.vendor.calcite.v1_20_0.com.google.common.collect.ImmutableList.of(
-                                ModifiedCalciteQueryPlanner.NonCumulativeCostImpl.SOURCE,
-                                RelMdNodeStats.SOURCE,
-                                rel.getCluster().getMetadataProvider())));
-
+    public RelOptCost getCumulativeCost(
+            BeamRelNode rel, ImmutableList<RelMetadataProvider> providers, RelMetadataQuery mq
+    ) {
+        rel.getCluster().setMetadataProvider(ChainedRelMetadataProvider.of(ImmutableList.of(
+                ModifiedCalciteQueryPlanner.NonCumulativeCostImpl.SOURCE,
+                RelMdNodeStats.SOURCE,
+                rel.getCluster().getMetadataProvider()
+        )));
         return mq.getCumulativeCost(rel);
     }
 }
