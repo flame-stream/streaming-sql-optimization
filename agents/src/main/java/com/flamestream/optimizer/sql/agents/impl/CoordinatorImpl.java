c4034964f782513a82599d53f70f78aabe96335c
chernokoz <chernokoz@hotmail.com>
2021-08-12 10:31:21 +0300

    add source-sink for graph statistics

--- agents/src/main/java/com/flamestream/optimizer/sql/agents/impl/CoordinatorImpl.java
+++ agents/src/main/java/com/flamestream/optimizer/sql/agents/impl/CoordinatorImpl.java
@@ -11,20 +11,21 @@ import org.apache.beam.sdk.extensions.sql.impl.rel.BeamRelNode;
 import org.apache.beam.sdk.io.Read;
 import org.apache.beam.sdk.io.UnboundedSource;
 import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.transforms.windowing.Window;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PCollectionTuple;
-import org.apache.beam.sdk.values.PInput;
-import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.values.*;
 import org.apache.beam.vendor.calcite.v1_20_0.org.apache.calcite.plan.RelOptCost;
 import org.apache.beam.vendor.calcite.v1_20_0.org.apache.calcite.rel.metadata.RelMetadataProvider;
 import org.checkerframework.checker.nullness.compatqual.NonNullType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -161,6 +162,119 @@ public class CoordinatorImpl implements Coordinator {
         SourceWithSchema(UnboundedSource<Row, @NonNullType ? extends UnboundedSource.CheckpointMark> source, Schema schema) {
             this.source = source;
             this.schema = schema;
+        }
+    }
+
+    public static class MessageSender {
+        private String messageToSend;
+        private InetSocketAddress address;
+
+        public MessageSender(String messageToSend, InetSocketAddress address) {
+            this.messageToSend = messageToSend;
+            this.address = address;
+        }
+
+        public void send() {
+            try {
+                SocketChannel client = SocketChannel.open(address);
+                log("Connecting to Server on " + address);
+
+                byte[] message = messageToSend.getBytes();
+                ByteBuffer buffer = ByteBuffer.wrap(message);
+                client.write(buffer);
+
+                log("sending: " + messageToSend);
+                buffer.clear();
+                client.close();
+            } catch (Exception e) {
+                log("Exception: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        private void log(String str) {
+            System.out.println(str);
+        }
+    }
+
+    public static class StatsOutput extends PTransform<PCollection<Row>, PDone> {
+        @Override
+        public PDone expand(PCollection<Row> input) {
+            input.apply(ParDo.of(new DoFn<Row, Void>() {
+                @ProcessElement
+                public void processElement(ProcessContext c, BoundedWindow window) {
+                    MessageSender sender = new MessageSender(
+                            c.element().toString(),
+                            new InetSocketAddress("localhost", 1234)
+                    );
+                    sender.send();
+                }
+            }));
+            return PDone.in(input.getPipeline());
+        }
+    }
+
+    public static class NIOServer implements Runnable {
+        public String result;
+
+        @Override
+        @SuppressWarnings("unused")
+        public void run() {
+
+            Selector selector;
+            ServerSocketChannel serverSocket;
+            InetSocketAddress address = new InetSocketAddress("localhost", 1111);
+            try {
+                selector = Selector.open();
+                serverSocket = ServerSocketChannel.open();
+                serverSocket.bind(address);
+                serverSocket.configureBlocking(false);
+
+                int ops = serverSocket.validOps();
+                SelectionKey selectKy = serverSocket.register(selector, ops, null);
+
+                for (;;) {
+                    selector.select();
+
+                    Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> selectedKeysIterator = selectedKeys.iterator();
+
+                    while (selectedKeysIterator.hasNext()) {
+                        SelectionKey key = selectedKeysIterator.next();
+                        selectedKeysIterator.remove();
+
+                        if (key.isAcceptable()) {
+                            SocketChannel client = serverSocket.accept();
+
+                            client.configureBlocking(false);
+                            client.register(selector, SelectionKey.OP_READ);
+                            log("Connection Accepted: " + client.getLocalAddress() + "\n");
+
+                        } else if (key.isReadable()) {
+
+                            SocketChannel client = (SocketChannel) key.channel();
+
+                            ByteBuffer buffer = ByteBuffer.allocate(256);
+
+                            if (client.read(buffer) < 0) {
+                                key.cancel();
+                                client.close();
+                                continue;
+                            }
+                            result = new String(buffer.array()).trim();
+
+                            log("Message received: " + result);
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void log(String str) {
+            System.out.println(str);
         }
     }
 }
